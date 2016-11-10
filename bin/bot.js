dbd7d29fc0bc37174fabb795aefdaa5edd216e53
Jonathan Ginn <jon@wearebase.com>
2016-11-10 13:57:59 +0000

    benedict!

--- bin/bot.js
+++ bin/bot.js
@@ -15,16 +15,13 @@ var NorrisBot = require('../lib/norrisbot');
  *
  *  BOT_API_KEY : the authentication token to allow the bot to connect to your slack organization. You can get your
  *      token at the following url: https://<yourorganization>.slack.com/services/new/bot (Mandatory)
- *  BOT_DB_PATH: the path of the SQLite database used by the bot
  *  BOT_NAME: the username you want to give to the bot within your organisation.
  */
 var token = process.env.BOT_API_KEY || require('../token');
-var dbPath = process.env.BOT_DB_PATH;
 var name = process.env.BOT_NAME;
 
 var norrisbot = new NorrisBot({
     token: token,
-    dbPath: dbPath,
     name: name
 });
 
