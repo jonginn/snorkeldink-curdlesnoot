6058d80e912db03beec534347978e65d81b37d44
Luke Wakeford <luke.wakeford@wearebase.com>
2016-12-01 11:07:05 +0000

    Adding some code to track reactions, posting only to bot-testing atm.

--- bin/bot.js
+++ bin/bot.js
@@ -17,8 +17,8 @@ var NorrisBot = require('../lib/norrisbot');
  *      token at the following url: https://<yourorganization>.slack.com/services/new/bot (Mandatory)
  *  BOT_NAME: the username you want to give to the bot within your organisation.
  */
-var token = process.env.BOT_API_KEY || require('../token');
-var name = process.env.BOT_NAME;
+var token = require('../token');
+var name = 'benedictbot';
 
 var norrisbot = new NorrisBot({
     token: token,
